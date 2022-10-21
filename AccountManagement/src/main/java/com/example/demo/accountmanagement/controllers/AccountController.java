919851a47738a8928eabc56922a54b4c21512780
Cauelb97 <caueleal_97@hotmail.com>
2022-10-21 16:42:31 -0300

    First commit

---
+++ AccountManagement/src/main/java/com/example/demo/accountmanagement/controllers/AccountController.java
@@ -0,0 +1,15 @@
+package com.example.demo.accountmanagement.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+	@GetMapping("/management")
+	public String status() {
+		return "Account created";
+	}
+}
